a95459ef86f1cc89c928621c39b3c32351ae7622
4lve <alve.jeansson@gmail.com>
2026-02-05 14:58:29 +0100

    Add fluid on class

--- src/main/kotlin/com/steelextractor/extractors/Classes.kt
+++ src/main/kotlin/com/steelextractor/extractors/Classes.kt
@@ -7,6 +7,7 @@ import com.steelextractor.SteelExtractor
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.server.MinecraftServer
 import net.minecraft.world.item.BlockItem
+import net.minecraft.world.level.block.LiquidBlock
 import org.slf4j.LoggerFactory
 
 class Classes : SteelExtractor.Extractor {
@@ -26,6 +27,13 @@ class Classes : SteelExtractor.Extractor {
 
             blockJson.addProperty("name", name)
             blockJson.addProperty("class", block.javaClass.simpleName)
+
+            if (block is LiquidBlock) {
+                val fluidField = LiquidBlock::class.java.getDeclaredField("fluid")
+                fluidField.isAccessible = true
+                val fluid = fluidField.get(block) as net.minecraft.world.level.material.FlowingFluid
+                blockJson.addProperty("fluid", BuiltInRegistries.FLUID.getKey(fluid)?.path)
+            }
 
             blocksJson.add(blockJson)
         }
